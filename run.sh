9dd522a0130bee92f1f9506845e0767284986d9b
Colin Rymer <colin.rymer@gmail.com>
2014-09-28 17:59:15 -0400

    fix variable assignment typo

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip=ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 sed "s/alt-host=127.0.0.1/alt-host=$ip/" -i ~/datomic/free-transactor.properties
 
 /root/datomic/bin/transactor /root/datomic/free-transactor.properties
